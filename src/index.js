65ac98184ff5d138e0351d3434f4d78e1c87ddc2
VJ <solankivj94@gmail.com>
2020-12-28 12:42:59 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {mod} from "./util";
+
+mod.sum(1,1)
+const packkker = () => "ddddd"
+console.log(packkker())
---
+++ src/util.js
@@ -0,0 +1,7 @@
+const sum = (a,b) => a+b
+const multi = (a,b) => a*b
+
+export const mod = {
+  sum,
+  multi
+}
