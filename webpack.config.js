5b0d4bc0aaa8ec06bdc40fcf54afd4312a84ea61
VJ <solankivj94@gmail.com>
2020-12-31 10:14:45 +0530

    add gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,3 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
+  
 }
